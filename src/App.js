e7ccfe68c83f90c90b90c0fcfb2f2a5a40e18c62
rohan_mittal <rohanmittal1366@gmail.com>
2021-06-17 01:04:34 +0530

    fetch api of countries and set into component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  useEffect(() => {
+    const getCountryData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountryData();
+  }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="app__header">
+        <h1>Covid 19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+
+      {/* Title + select inout dropdown field */}
+
+      {/* InfoBox */}
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
     </div>
   );
 }
